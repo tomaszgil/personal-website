e9e35bf1bab6200ada2387a7ac0d80fe9dd73a22
Tomasz Gil <tomaszgil.pom@gmail.com>
2017-04-20 12:48:34 +0200

    Add animations for navigation

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -4,7 +4,49 @@ $(document).ready(function() {
 
 function handleMobileNavigation() {
   $("#menu-button").on('click', function() {
-    $("#menu").toggleClass("header--menu__mobile");
-    $(this).toggleClass("header--mobile-menu-button__close");
+    if($("#menu").hasClass("header--menu__mobile")) {
+      hideMobileNavigation();
+    } else {
+      showMobileNavigation();
+    }
   });
+
+  $('.header--menu--option').on('click', function (event) {
+    var target = $($(this).attr('href'));
+    event.preventDefault();
+    $('html, body').animate({
+      scrollTop: target.offset().top
+    }, 1000);
+    if($("#menu").hasClass("header--menu__mobile")) {
+      hideMobileNavigation();
+    }
+  });
+
+  $(window).resize(function() {
+    if($(this).width() > 760) {
+      $("#menu").show();
+    }
+  });
+
+  $(document).keyup(function(e) {
+    if(e.keyCode == 27) hideMobileNavigation();
+  });
+}
+
+function hideMobileNavigation() {
+  var menu = $("#menu");
+  var menuButton = $("#menu-button");
+  menuButton.removeClass("header--mobile-menu-button__close");
+  menu.fadeOut(300, function() {
+    menu.removeClass("header--menu__mobile");
+  });
+}
+
+function showMobileNavigation() {
+  var menu = $("#menu");
+  var menuButton = $("#menu-button");
+  menuButton.addClass("header--mobile-menu-button__close");
+  menu.hide();
+  menu.addClass("header--menu__mobile");
+  menu.fadeIn(300);
 }
